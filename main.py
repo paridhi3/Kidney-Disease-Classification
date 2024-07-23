2fb64fb1eeedd4a1402ae7abe4b781620b411da2
Paridhi <pari9901@gmail.com>
2024-07-23 23:08:41 +0530

    prepare_base_model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from KDClassification import logger
 from KDClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from KDClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from KDClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from KDClassification.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from KDClassification.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -19,18 +19,16 @@ except Exception as e:
 
 
 
-
-
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
